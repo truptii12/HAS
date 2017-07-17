5e66b034bb36ed3aa3da0ec3cc2bcbebe799dfdc
trupti19 <trupti.its.me@gmail.com>
2017-07-17 17:53:44 +0000

    u

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170712175511) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "kulkarni.trupti.v.19@gmail.com", null: false
-    t.string   "encrypted_password",     default: "1",                    null: false
+    t.string   "encrypted_password",     default: "Kulkas12025",                    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
